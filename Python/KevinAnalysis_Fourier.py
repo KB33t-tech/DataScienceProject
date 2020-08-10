32eb400483aca9505c262b90798c9097ec8fb6ba
Kevin Beja <kdb4@sfu.ca>
2020-08-09 23:47:29 -0700

    added more to the fourier plot

--- Python/KevinAnalysis_Fourier.py
+++ Python/KevinAnalysis_Fourier.py
@@ -7,6 +7,7 @@ from scipy.fft import fft, ifft, fftfreq
 import seaborn
 
 def main(in_directory):
+
     seaborn.set()
     data = pd.read_csv(in_directory)
     time = data['time']
@@ -14,6 +15,10 @@ def main(in_directory):
     T = 1/Freq
     L= int(data.shape[0])
 
+    #butter fractions
+    strin_but= 2/Freq
+    good_but = 10/Freq
+    lax_but =  25/Freq
     #give columns variable names
     x = data['gFx']
     y = data['gFy']
@@ -27,13 +32,13 @@ def main(in_directory):
     i = freq>0
 
     #plting unfiltered data
-    plt.subplot(1,3,3)
+    plt.subplot(2,3,1)
     plt.plot(freq[i],X_PSD_o[i])
 
     #fourier with butterworth filter on x
     # adjust Butter filter until we get 1 peak with minimal effect on power spectral density 
 
-    b, a = signal.butter(3, 0.1, btype='lowpass', analog=False)
+    b, a = signal.butter(3, strin_but, btype='lowpass', analog=False)
     filtered_x = signal.filtfilt(b, a, x)
     filtered_x_fft = fft(filtered_x)
     filtered_X_PSD_o = abs(filtered_x_fft)
@@ -42,31 +47,24 @@ def main(in_directory):
     plt.plot(filtered_freq[i],filtered_X_PSD_o[i])
     plt.legend(labels=('raw data','filtered'))
     plt.ylabel("Power Spectral Density")
-    plt.title("Fourier transform with critical frequency of 0.1")
+    plt.title("Fourier transform with critical frequency of %.3f" %strin_but)
     plt.xlabel("Frequency in Hz")
     plt.xlim(0,20)
 
-    #plting filtered data too stringently
-    plt.subplot(1,3,1)
-    plt.xlim(0,20)
-    plt.plot(freq[i],X_PSD_o[i])
-    b, a = signal.butter(3, 0.02, btype='lowpass', analog=False)
-    filtered_x = signal.filtfilt(b, a, x)
-    filtered_x_fft = fft(filtered_x)
-    filtered_X_PSD_o = abs(filtered_x_fft)
-    filtered_freq = fftfreq(L,d = T)
-    filtered_i = freq>0
-    plt.plot(filtered_freq[i],filtered_X_PSD_o[i])
-    plt.legend(labels=('raw data','filtered'))
-    plt.xlabel("Frequency in Hz")
-    plt.ylabel("Power Spectral Density")
-    plt.title("Fourier transform with critical frequency of 0.02")
+    #plt scatter below
+    plt.subplot(2,3,4)
+    plt.plot(time,x,'b.')
+    plt.plot(time,filtered_x,'r-')
+    plt.ylabel('acceleration in gs (1g ~= 9.8m/s^2')
+    plt.xlabel('time in seconds')
+    plt.legend(labels=('raw','filtered'))
+    plt.xlim(500,503)
 
-    #plting filtered data too stringently
-    plt.subplot(1,3,2)
+    #plting filtered just right
+    plt.subplot(2,3,2)
     plt.xlim(0,20)
     plt.plot(freq[i],X_PSD_o[i])
-    b, a = signal.butter(3, 0.05, btype='lowpass', analog=False)
+    b, a = signal.butter(3, good_but, btype='lowpass', analog=False)
     filtered_x = signal.filtfilt(b, a, x)
     filtered_x_fft = fft(filtered_x)
     filtered_X_PSD_o = abs(filtered_x_fft)
@@ -76,7 +74,39 @@ def main(in_directory):
     plt.legend(labels=('raw data','filtered'))
     plt.xlabel("Frequency in Hz")
     plt.ylabel("Power Spectral Density")
-    plt.title("Fourier transform with critical frequency of 0.05")
+    plt.title("Fourier transform with critical frequency of %.2f" %good_but)
+
+    #plt scatter below
+    plt.subplot(2,3,5)
+    plt.plot(time,x,'b.')
+    plt.plot(time,filtered_x,'r-')
+    plt.ylabel('acceleration in gs (1g ~= 9.8m/s^2')
+    plt.xlabel('time in seconds')
+    plt.legend(labels=('raw','filtered'))
+    plt.xlim(500,503)
+
+    #plting filtered data with not enough stringency
+    plt.subplot(2,3,3)
+    plt.xlim(0,20)
+    plt.plot(freq[i],X_PSD_o[i])
+    b, a = signal.butter(3, lax_but, btype='lowpass', analog=False)
+    filtered_x = signal.filtfilt(b, a, x)
+    filtered_x_fft = fft(filtered_x)
+    filtered_X_PSD_o = abs(filtered_x_fft)
+    filtered_freq = fftfreq(L,d = T)
+    filtered_i = freq>0
+    plt.plot(filtered_freq[i],filtered_X_PSD_o[i])
+    plt.legend(labels=('raw data','filtered'))
+    plt.xlabel("Frequency in Hz")
+    plt.ylabel("Power Spectral Density")
+    plt.title("Fourier transform with critical frequency of %0.2f" %lax_but)
+
+    #plt scatter below
+    plt.subplot(2,3,6)
+    plt.plot(time,x,'b.')
+    plt.plot(time,filtered_x,'r-')
+
+    plt.xlim(500,503)
 
     plt.show()
   
